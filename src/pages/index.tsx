66f107bd15fb088386125bceebd0fe46d956f297
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-20 11:01:31 -0300

    chore: change index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
 import { useRouter } from 'next/router';
 
-import { useAuth } from '../contexts/AuthContext';
-
 export default function HomePage() {
   const router = useRouter();
-  const { authenticated, login } = useAuth();
-
-  function handleLogin() {
-    login();
-    router.push('/dashboard');
-  }
-
-  // eslint-disable-next-line no-console
-  console.log(`public env message: ${process.env.NEXT_PUBLIC_ENV_MESSAGE}`);
 
   return (
     <div>
@@ -20,19 +9,7 @@ export default function HomePage() {
 
       <p>Description Home page</p>
 
-      <button onClick={() => router.push('/contact')}>Contact</button>
-
-      <p>User is logged: {authenticated ? 'Logged' : 'Not Logged'}</p>
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={() => router.push('/partners')}>Partners</button>
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  // eslint-disable-next-line no-console
-  console.log(`private env message: ${process.env.PRIVATE_ENV_MESSAGE}`);
-
-  return {
-    props: {},
-  };
-};
